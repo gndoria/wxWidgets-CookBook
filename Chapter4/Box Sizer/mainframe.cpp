e979cf40288563b2e65c430ff6e35a1d261b49c0
gideon <gndoria@gmail.com>
2017-09-28 13:34:19 +0300

    Added a recipe of a wxBoxSizer

---
+++ Chapter4/Box Sizer/mainframe.cpp
@@ -0,0 +1,65 @@
+/**
+@author Gideon Ndoria
+@license lgpl v3
+@file mainframe.cpp
+@brief The implementation of the applications mainframe
+*/
+#include "mainframe.h"
+#include <wx/aboutdlg.h>/**This header is not included in wx.h.It declares wxAboutDialogInfo and  wxAboutBox*/
+
+/**
+Implement the applications main frame
+*/
+MainFrame::MainFrame(const wxString &title)
+	: wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE){
+	Center();/**Place the applications frame at the center of the screen*/
+	////////////////////////////////////////////////////////////////////////////
+	menuBar = new wxMenuBar;/**Initialise the menuBar.*/
+	this->SetMenuBar(menuBar);/**Set the frames menu bar*/
+
+	////////////////////////////////////////////////////////////////////////////
+	fileMenu = new wxMenu;/**Initialise the fileMenu*/
+	
+	fileMenu->Append(wxID_EXIT, _("&Quit"), _("Quit the application"));/**Add a menu item with a stock id and help text*/
+
+	////////////////////////////////////////////////////////////////////////////
+	helpMenu = new wxMenu;/**Initialise the helpMenu*/
+
+	helpMenu->Append(ID_ABOUT, _("&About	F1"), _("Show about dialog"));/**Add a menu item with a custom id and an accelerator of F1*/
+
+	////////////////////////////////////////////////////////////////////////////
+	menuBar->Append(fileMenu, _("&File"));/**Add the fileMenu to the menuBar*/
+	menuBar->Append(helpMenu, _("&Help"));/**Add the helpMenu to the menuBar*/
+
+	////////////////////////////////////////////////////////////////////////////
+	mainSizer = new wxBoxSizer(wxVERTICAL);/**Initialise mainsizer as an instance of a boxsizer and set its orientation*/
+	this->SetSizer(mainSizer);
+
+	mainPanel = new wxPanel(this);/**Initialise the panel and set its' parent to this window*/
+	mainSizer->Add(mainPanel);/**Add the panel to mainSizer*/
+
+	contentSizer = new wxBoxSizer(wxHORIZONTAL);/**Initialise contentSizer as an instance of a boxsizer and set its orientation.
+	This is the sizer we will be adding out controlls to*/
+	mainPanel->SetSizer(contentSizer);/**Set contentSizer as mainPanels' Sizer*/
+
+	////////////////////////////////////////////////////////////////////////////
+	CreateStatusBar();/**Create the status bar*/
+
+}
+
+/**Implement the event table
+Take special notes of the signatures
+*/
+wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
+	EVT_MENU(wxID_EXIT, MainFrame::onClose)/**Handle the Quit menu event*/
+	EVT_MENU(ID_ABOUT, MainFrame::onAbout)/**Handle the Quit menu event*/
+wxEND_EVENT_TABLE()
+
+void MainFrame::onClose(wxCommandEvent &event){
+	Close();//Close the application
+}
+
+void MainFrame::onAbout(wxCommandEvent &event){
+	wxMessageBox(_("A demo application for wxWidgets 3.0 cookbook.\n(C) Gideon Ndoria 2017"),
+		_("About The Application"));/**Show the about dialog*/
+}
---
+++ Chapter4/Box Sizer/mainframe.h
@@ -0,0 +1,42 @@
+/**
+@author Gideon Ndoria
+@license lgpl v3
+@file mainframe.h
+@brief The class definition of the applications mainframe
+*/
+
+#pragma once/**A feature of c++11, used inplace of inclusion guards*/
+
+#include <wx/wx.h>/**This header includes the most commonly used wxWidgets classes*/
+
+/**
+MainFrame class
+@brief Declares the applications mainframe
+*/
+class MainFrame : public wxFrame{
+	public:
+		enum id{
+			ID_ABOUT/**Id for the about menuitem*/
+		};
+		/**
+		The constructor
+		@param const wxString &title The only thing we need to customise at the moment is the title
+		*/
+		MainFrame(const wxString &title);
+	private:
+		/**Declare a pointer to the menubar object.
+		We assign it to nullptr instead of {wxMenuBar *menuBar;} to prevent the copy constructor from being called*/
+		wxMenuBar *menuBar = nullptr;
+		wxMenu *fileMenu ,*helpMenu = nullptr;/**Declare the menus*/
+
+		wxSizer *mainSizer, *contentSizer;/**Declare a generic sizer.This is to allow you to specialise it to a diffrent sizer type as required.
+		For example we could initialise contentSizer as a boxsizer now and if our ui design changes i.e. If we decide we want a flexgrid sizer
+		instead, the only change would only occur in our initalisation code in mainframe.cpp*/
+
+		wxPanel *mainPanel;/**The panel will be added to the mainSizer.This is to cover the window background in microsoft windows*/
+
+		void onClose(wxCommandEvent &event);/**Declare the function to handle the close event*/
+		void onAbout(wxCommandEvent &event);/**Declare the function to handle the about event*/
+
+		wxDECLARE_EVENT_TABLE();/**Declare the event table*/
+};
